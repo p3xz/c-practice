b0a7ffdbf1e5aa25c97dd6c61dc02d019f167d90
Namish-yadav <namishyadavv@gmail.com>
2026-08-05 18:04:48 +0530

    Added files

---
+++ Basics/hello.c
@@ -0,0 +1,8 @@
+//C code to print Hello World
+#include <stdio.h>  
+
+int main(void)
+{
+    printf("Hello World\n");
+    return 0;
+}
---
+++ Basics/question.c
@@ -0,0 +1,9 @@
+//program to ask user to enter his name 
+#include <stdio.h>
+
+int main(void)
+{
+
+    string question = get_string("What is your name? ");
+    printf("hello, %s/n" question);
+}
